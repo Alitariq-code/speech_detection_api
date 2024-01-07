57f9716cd2faeb60445b36a89f7b741989ef1d0e
Ali_Tariq <82971663+Alitariq-code@users.noreply.github.com>
2024-01-07 23:32:15 +0500

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,14 +1,86 @@
+
 import speech_recognition as sr
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 from difflib import SequenceMatcher
+
+import re
+import pandas as pd
+
+from fuzzywuzzy import fuzz
+
+def remove_newlines(text):
+    cleaned_text = text.replace('\n', ' ')
+    return cleaned_text
+def remove_punctuation(text):
+    # Remove commas and periods
+    cleaned_text = text.replace(',', '').replace('.', ' ')
+    cleaned_text = cleaned_text.replace('“', '').replace('”', ' ')
+    return cleaned_text
+def track_deleted_words(original_lines, spoken_lines):
+    print(original_lines,spoken_lines)
+    original_lines = original_lines.split()
+    spoken_lines = spoken_lines.split()
+    print('inside deleted',type(original_lines),type(spoken_lines))
+    # Initialize lists to store deleted words and their positions
+    deleted_words = []
+    deleted_positions = []
+    
+
+    # Iterate through each word in the original text
+    for index, word in enumerate(original_lines):
+        # print(word)
+        # Check if the word is missing in the spoken text
+        word = word.strip()  
+        if word not in spoken_lines:
+            # print("worddddddsssssssssssssss-----------------------",word)
+            deleted_words.append(word)
+            deleted_positions.append(index)
+            
+    # print(deleted_positions)
+    # print("issue",deleted_positions,deleted_words)
+    return deleted_words, deleted_positions
+
+
+def track_inserted_words(original_lines, spoken_lines):
+
+    original_words = original_lines.split()
+    spoken_words = spoken_lines.split()
+    # print("okok",spoken_words)
+    # print("okoksss",original_words)
+    # Initialize lists to store inserted words and their positions
+    
+    inserted_words = []
+    inserted_positions = []
+    # print("insertion------")
+    # print(spoken_words)
+    # print(original_words)
+
+
+    # Iterate through each word in the spoken text
+    for index, word in enumerate(spoken_words):
+        # print("orgi",original_words)
+
+        # Check if the word is not in the original text
+        if word not in original_words:
+            inserted_words.append(word)
+            inserted_positions.append(index)
+    print("posi",inserted_words)
+    return inserted_words, inserted_positions
+
+
+def find_most_similar(word, word_list):
+    similarities = [(w, SequenceMatcher(None, word, w).ratio()) for w in word_list]
+    similarities.sort(key=lambda x: x[1], reverse=True)
+    return similarities[0]
 
 def transcribe_audio(audio_file_path, language="nl-NL"):
     """
     Transcribes audio from a given file path using Google Speech Recognition.
 
     Parameters:
-    - audio_file_path (str): Path to the audio file.
+    - audio_file_path (str): Path to thprint("okok",spoken_words)
+    # print("okoksss",original_words)e audio file.
     - language (str): Language code for transcription (default is "nl-NL").
 
     Returns:
@@ -116,71 +188,189 @@ def analyze_audio(file_path):
         "long_pauses": long_pauses
     }
 
-def compare_lines(original_lines, spoken_lines, similarity_threshold=0.1):
-    """
-    Compare original and spoken lines for deleted, inserted, substituted, and repeated words.
 
-    Parameters:
-    - original_lines (str): Original text.
-    - spoken_lines (str): Spoken text.
-    - similarity_threshold (float): Similarity threshold for considering lines similar (default is 0.1).
+def compare_lines(original_text, spoken_text):
+    def preprocess_text(text):
+        return re.sub(r'[\r\n]+', ' ', re.sub(r'[^\w\s]', ' ', text)).lower()
+    def align_texts(original, spoken):
+        sequence_matcher = SequenceMatcher(None, original, spoken)
+        operations = []
+        for opcode in sequence_matcher.get_opcodes():
+            tag, i1, i2, j1, j2 = opcode
+            if tag == 'delete':
+                operations.append(['Deletion', ' '.join(original[i1:i2]), i1, i2-1, '', '', '', '', '', 0])
+            elif tag == 'insert':
+                before_context = ' '.join(spoken[max(0, j1-1):j1])
+                after_context = ' '.join(spoken[j2:min(len(spoken), j2+1)])
+                operations.append(['Insertion', '', '', '', ' '.join(spoken[j1:j2]), j1, j2-1, before_context, after_context, 0])
+            elif tag == 'replace':
+                original_segment = ' '.join(original[i1:i2])
+                spoken_segment = ' '.join(spoken[j1:j2])
+                similarity = fuzz.ratio(original_segment, spoken_segment)
+                operations.append(['Substitution', original_segment, i1, i2-1, spoken_segment, j1, j2-1, '', '', similarity])
+        return operations
+    def process_substitutions(df):
+        new_rows = []
+        for index, row in df.iterrows():
+            if row['Operation'] == 'Substitution' and row['Similarity'] < 40:
+                # Splitting the row into two parts: Insertion and Deletion
+                insertion_row = row.copy()
+                insertion_row['Operation'] = 'Insertion'
+                deletion_row = row.copy()
+                deletion_row['Operation'] = 'Deletion'
+                new_rows.extend([insertion_row, deletion_row])
+            else:
+                new_rows.append(row)
+        return pd.DataFrame(new_rows)
+    # Preprocessing texts
+    preprocessed_original_text = preprocess_text(original_text).split()
+    preprocessed_spoken_text = preprocess_text(spoken_text).split()
+    # Aligning and getting differences
+    differences = align_texts(preprocessed_original_text, preprocessed_spoken_text)
+    # Creating DataFrame
+    df = pd.DataFrame(differences, columns=['Operation', 'Original Segment', 'Original Start', 'Original End', 'Spoken Segment', 'Spoken Start', 'Spoken End', 'Before Context', 'After Context', 'Similarity'])
+    # Process substitutions
+    processed_df = process_substitutions(df)
+    # Function to select columns based on the operation
+    def select_columns(row):
+        if row['Operation'] in ['Insertion', 'Substitution']:
+            return pd.Series([row['Operation'], row['Spoken Segment'], row['Spoken Start'], row['Spoken End']])
+        elif row['Operation'] == 'Deletion':
+            return pd.Series([row['Operation'], row['Original Segment'], row['Original Start'], row['Original End']])
+    # Define a function to determine the class based on the color
+    def determine_class(color):
+        if color == 'Red':
+            return 'Deletion'
+        elif color == 'Yellow':
+            return 'Substitution'
+        elif color == 'Pink':
+            return 'Insertion'
+        elif color == 'Green':
+            return 'Correct'
+        else:
+            return 'Unknown'
 
-    Returns:
-    - tuple: Lists of deleted, inserted, substituted, and repeated words.
-    """
-    original_lines = original_lines.lower()
-    spoken_lines = spoken_lines.lower()
+    # Function to generate ID and word pairs
+    def generate_id_word_pairs(row):
+        start = row['Start/Spoken Start']
+        end = row['End/Spoken End']
+        segment = row['Segment/Original Segment']
+        # Split the segment into words
+        words = segment.split()
+        # Generate ID and word pairs
+        id_word_pairs = [(start + i, word) for i, word in enumerate(words)]
+        return id_word_pairs
+    try:
+        # Applying the function to each row and reformating DataFrame
+        reformatted_df = processed_df.apply(select_columns, axis=1)
+        reformatted_df.columns = ['Operation', 'Segment/Original Segment', 'Start/Spoken Start', 'End/Spoken End']
+        reformatted_df=reformatted_df.reset_index(drop=True)
+        insert=reformatted_df[reformatted_df['Operation']=='Insertion']
+        delete=reformatted_df[reformatted_df['Operation']=='Deletion']
+        subt=reformatted_df[reformatted_df['Operation']=='Substitution']
+        # Apply the function to each row and flatten the list
+        try:
+            id_word_pairs_list = insert.apply(generate_id_word_pairs, axis=1).explode()
+            # Create a new dataframe with ID and word columns
+            insert_df = pd.DataFrame(id_word_pairs_list.tolist(), columns=['ID', 'word'])
+        except:
+            insert_df = pd.DataFrame(columns=['ID', 'word'])
+        try:
+            # Apply the function to each row and flatten the list
+            id_word_pairs_list = delete.apply(generate_id_word_pairs, axis=1).explode()
+            # Create a new dataframe with ID and word columns
+            delete_df = pd.DataFrame(id_word_pairs_list.tolist(), columns=['ID', 'word'])
+        except:
+            delete_df= pd.DataFrame(columns=['ID', 'word'])
+        try:
+            id_word_pairs_list = subt.apply(generate_id_word_pairs, axis=1).explode()
+            # Create a new dataframe with ID and word columns
+            subt_df = pd.DataFrame(id_word_pairs_list.tolist(), columns=['ID', 'word'])
+        except:
+            subt_df= pd.DataFrame(columns=['ID', 'word'])
+        # Iterate over each row in the Substitution DataFrame
+        substituted_words = []
+        delete_words = []
+        insert_words = []
+        for index, row in subt_df.iterrows():
+            substituted_word = {
+            'ID': row['ID'],
+            'Word': row['word'],
+        }
+            substituted_words.append(substituted_word)
+        for index, row in delete_df.iterrows():
+            delete_word = {
+            'ID': row['ID'],
+            'Word': row['word'],
+        }
+            delete_words.append(delete_word)
+        for index, row in insert_df.iterrows():
+            insert_word = {
+            'ID': row['ID'],
+            'Word': row['word'],
+        }
+            insert_words.append(insert_word)
+    # ...
+        print("subWOrds",substituted_words)
+        print("deleted",delete_words)
+        print("insert",insert_words)
+    except:
+        delete_df= pd.DataFrame(columns=['ID', 'word'])
+        subt_df= pd.DataFrame(columns=['ID', 'word'])
+        insert_df= pd.DataFrame(columns=['ID', 'word'])
+        substituted_words = []
+        delete_words = []
+        insert_words = []
+        for index, row in subt_df.iterrows():
+            substituted_word = {
+            'ID': row['ID'],
+            'Word': row['word'],
+        }
+            substituted_words.append(substituted_word)
+        for index, row in delete_df.iterrows():
+            delete_word = {
+            'ID': row['ID'],
+            'Word': row['word'],
+        }
+            delete_words.append(delete_word)
+        for index, row in insert_df.iterrows():
+            insert_word = {
+            'ID': row['ID'],
+            'Word': row['word'],
+        }
+            insert_words.append(insert_word)
+    # ...
+        print("subWOrds",substituted_words)
+        print("deleted",delete_words)
+        print("insert",insert_words)
+    spoken_df = pd.DataFrame({'ID': range(len(preprocessed_spoken_text)), 'word': preprocessed_spoken_text})
+    spoken_df['Color']='Green'
+    merged_df = pd.merge(spoken_df, subt_df, on='ID', how='left', suffixes=('_spoken', '_subt'))
+    merged_df['Color'] = merged_df['Color'].where(merged_df['word_subt'].isnull(), 'Yellow')
+    merged_df=merged_df[['ID','word_spoken','Color']]
+    merged_df = pd.merge(merged_df, insert_df, on='ID', how='left', suffixes=('', '_insr'))
+    merged_df['Color'] = merged_df['Color'].where(merged_df['word'].isnull(), 'Pink')
+    merged_df=merged_df[['ID','word_spoken','Color']]
+    delete_df['Color']='Red'
+    delete_df.columns=merged_df.columns
+    merged_df=pd.concat([merged_df,delete_df])
+    merged_df=merged_df.sort_values("ID")
+    merged_df=merged_df.reset_index().reset_index()
+    merged_df=merged_df[['level_0','word_spoken','Color']]
+    merged_df.columns=delete_df.columns
+    # Create a new column 'Class' based on the 'Color' column
+    merged_df['Class'] = merged_df['Color'].apply(determine_class)
+    merged = [] 
+    for index, row in merged_df.iterrows():
+            insert_word = {
+            'ID': row['ID'],
+            'Word': row['word_spoken'],
+            'Color': row['Color'],
+            'Class': row['Class'],
+        }
+            merged.append(insert_word)
+    return substituted_words,delete_words,insert_words,merged
 
-    original_lines = remove_newlines(original_lines)
-    original_lines = remove_punctuation(original_lines)
-    spoken_lines = remove_newlines(spoken_lines)
-    original_lines = original_lines.strip().split('\n')
-    spoken_lines = spoken_lines.strip().split('\n')
-
-    deleted_words = []
-    inserted_words = []
-    substituted_words = []
-    repeated_words = []
-
-    for spoken_line in spoken_lines:
-        max_similarity = 0
-        most_similar_original_line = None
-
-        # Calculate similarity for each original line
-        for original_line in original_lines:
-            similarity = SequenceMatcher(None, original_line, spoken_line).ratio()
-            if similarity > max_similarity:
-                max_similarity = similarity
-                most_similar_original_line = original_line
-
-        if max_similarity >= similarity_threshold:
-            original_words = most_similar_original_line.split()
-            spoken_words = spoken_line.split()
-
-            # Identify deleted words
-            deleted_words.extend([word for word in original_words if word not in spoken_words])
-
-            # Identify inserted words
-            inserted_words.extend([word for word in spoken_words if word not in original_words])
-
-            # Identify substituted words (original word not in deleted or inserted)
-            substituted_words.extend([(original_word, spoken_word) for original_word, spoken_word in zip(original_words, spoken_words) if original_word not in spoken_words])
-
-            # Identify consecutive repeated words
-            current_repeated_words = []
-            for word in spoken_words:
-                if spoken_words.count(word) > 1 and word not in current_repeated_words:
-                    current_repeated_words.append(word)
-                else:
-                    if len(current_repeated_words) > 1:
-                        repeated_words.extend(current_repeated_words)
-                    current_repeated_words = []
-
-    subt = list(dict(substituted_words).keys())
-    substituted_words = [word for word in subt if word not in inserted_words]
-    repeated_words = remove_duplicates(repeated_words)
-
-    return deleted_words, inserted_words, substituted_words, repeated_words
 
 def count_duplicate_lines(text_data):
     """
@@ -208,6 +398,11 @@ def count_duplicate_lines(text_data):
             seen_lines.add(line)
 
     return duplicate_count
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+# word_count = count_words(original_text)
 
 def count_skipped_lines(text_data):
     """
@@ -264,30 +459,18 @@ def remove_duplicates(word_list):
 
     return unique_words
 
-def remove_newlines(text):
-    """
-    Remove newline characters from the given text.
+# def remove_newlines(text):
+#     """
+#     Remove newline characters from the given text.
 
-    Parameters:
-    - text (str): Text.
+#     Parameters:
+#     - text (str): Text.
 
-    Returns:
-    - str: Text with newlines removed.
-    """
-    return text.replace('\n', '')
+#     Returns:
+#     - str: Text with newlines removed.
+#     """
+#     return text.replace('\n', '')
 
-def remove_punctuation(text):
-    """
-    Remove commas and periods from the given text.
-
-    Parameters:
-    - text (str): Text.
-
-    Returns:
-    - str: Text with commas and periods removed.
-    """
-    cleaned_text = text.replace(',', '').replace('.', '')
-    return cleaned_text
 
 def compare_segments(segment1, segment2):
     """
@@ -301,6 +484,25 @@ def compare_segments(segment1, segment2):
     - bool: True if the RMS of segment1 is greater than 80% of the RMS of segment2, else False.
     """
     return segment1.set_frame_rate(44100).set_channels(1).rms > segment2.set_frame_rate(44100).set_channels(1).rms * 0.8
+
+
+def find_repeated_words(text):
+    words = text.split()
+    repeated_words = []
+    for i in range(len(words) - 1):
+        if words[i] == words[i + 1]:
+            repeated_words.append(words[i])
+    return repeated_words
+
+
+
+def calculate_word_count_ratio(transcribed_text, original_text, max_ratio=100):
+    word_count_org = count_words(original_text)
+    word_count_transcribed = count_words(transcribed_text)
+
+    ratio = min(100*word_count_org / word_count_transcribed , word_count_transcribed* 100/word_count_org)
+    return ratio
+
 
 # Example usage:
 # transcribed_text = transcribe_audio("/home/alicode/Desktop/stock/AVI 4.wav")
